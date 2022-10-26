a51e6b0aa463b2fd578381a35d7357496e2c42af
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-10-26 12:42:00 +0200

    feat: docker startup automatic

--- DockerSetup/exec.bat
+++ DockerSetup/exec.bat
@@ -1,4 +1,4 @@
 docker stop kubernetes_workshop
 docker rm kubernetes_workshop
-docker build -t kubernetes_workshop ./
-docker run -d --privileged --name kubernetes_workshop kubernetes_workshop sleep infinity
+docker build -t renkelp/kubernetes_workshop ./
+docker run -d --privileged --name kubernetes_workshop renkelp/kubernetes_workshop sleep infinity
